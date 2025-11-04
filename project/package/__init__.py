4c5fea957c658a548ebf8be7981849956f0d0c4e
Silva Joao <Joao.Silva@tridonic.com>
2025-11-04 12:14:55 +0000

    shorten module name

--- project/package/__init__.py
+++ project/package/__init__.py
@@ -1,6 +1,6 @@
 import os.path
 
-from jl95terceira.pytools.envlib import vars,var
+from jl95.pytools.envlib import vars,var
 
 OPENSSL_HOME = var(name       ='openssl.home', 
                    description='the home of OpenSSL')
